74375b8f15dd58adf1d3935b2a729f66431b4822
yassine <PSR11616@students.ephec.be>
2023-07-07 10:05:45 +0200

    End of section 6

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from './_services/account.service';
 import { User } from './_models/user';
+import { ToastrModule } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
